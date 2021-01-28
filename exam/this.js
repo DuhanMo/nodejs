951b978a2c1382d1ad6b5aded06f9c25840f6f85
DuhanMo <devduhanmo@gmail.com>
2021-01-28 17:47:18 +0900

    2021/01/28 nodejs

---
+++ exam/this.js
@@ -0,0 +1,6 @@
+console.log(this);
+console.log(this === module.exports);
+console.log(this === exports);
+(function whatIsThis() {
+  console.log('function', this === exports, this === global);
+})();
