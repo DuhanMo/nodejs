2cc2b76999eea7d5a80d23ecb40146ed178ee0eb
DuhanMo <devduhanmo@gmail.com>
2021-02-15 17:20:16 +0900

    20210215 node study

--- ch6/6.1/learn-express/app.js
+++ ch6/6.1/learn-express/app.js
@@ -4,12 +4,19 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 dotenv.config();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -26,57 +33,21 @@ app.use(session({
   },
   name: 'session-cookie',
 }));
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send('Not Found~!');
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
-
-const multer = require('multer');
-const fs = require('fs');
-
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
-}
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads/');
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname);
-      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
-
-
-
-app.get('/upload', (req, res) => {
-  res.sendFile(path.join(__dirname, 'multipart.html'));
-});
-app.post('/upload',
- upload.fields([{name: 'image1'}, {name: 'image2'}]), 
- (req, res) => {
-  console.log(req.files, req.body);
-  res.send('ok');
-});
-
-app.get('/', (req, res, next) => {
-  console.log('GET / 요청에서만 실행됩니다.');
-  next();
-}, (req, res) => {
-  throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
-});
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
