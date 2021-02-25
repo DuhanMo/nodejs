3e03bd99ad76354427e2c1bef7cd4b0935cedf92
DuhanMo <devduhanmo@gmail.com>
2021-02-25 16:22:15 +0900

    1시간만에 끝내는 nodejs 입문

--- classOfdev/server.js
+++ classOfdev/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 const server = app.listen(3000, () => {
@@ -9,9 +10,33 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.get('/about', (req, res) => {
-  res.send('about page');
+  res.render('about.html');
 });
-
 app.get('/', (req, res) => {
   res.render('index.html');
 });
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '1111',
+  database        : 'nodejs'
+});
+ 
+app.get('/db', (req, res) => {
+  pool.getConnection(function(err, connection) {
+    if (err) throw err; // not connected!
+   
+    // Use the connection
+    connection.query('SELECT * FROM users', function (error, results, fields) {
+      // When done with the connection, release it.
+      connection.release();
+   
+      // Handle error after the release.
+      if (error) throw error;
+   
+      // Don't use the connection here, it has been returned to the pool.
+    });
+  });
+})
