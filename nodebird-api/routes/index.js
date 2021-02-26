443c14f10e706759c8eee26ce78bf6b5a77f1eda
DuhanMo <devduhanmo@gmail.com>
2021-02-26 12:18:39 +0900

    commit

--- nodebird-api/routes/index.js
+++ nodebird-api/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
-const { User, Domain } = require('../models');
+const { User, Domain } =require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -8,16 +8,16 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const user = await User.findOne({
-      where: { id: req.user && req.user.id || null },
-      include: { model: Domain },
+      where : { id: req.user && req.user.id || null },
+      include: { model : Domain },
     });
     res.render('login', {
       user,
       domains: user && user.Domains,
     });
-  } catch (err) {
-    console.error(err);
-    next(err);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
@@ -29,11 +29,13 @@ router.post('/domain', isLoggedIn, async (req, res, next) => {
       type: req.body.type,
       clientSecret: uuidv4(),
     });
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    next(err);
+    res.render('/');
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
 module.exports = router;
+
+
